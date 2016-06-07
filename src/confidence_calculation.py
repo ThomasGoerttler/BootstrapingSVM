ce2041a64380c52375051c17756948083f0d2a36
Thomas Goerttler <thomas.goerttler@gmail.com>
2016-06-07 15:03:29 +0200

    Added calculation of the variance of the svm

--- src/confidence_calculation.py
+++ src/confidence_calculation.py
@@ -1,25 +1,31 @@
 import math
+import statistics
 
-def mean(array):
-    sum = 0
-    for i in range(len(array)):
-        sum += array[i]
-    return (sum/len(array))
-    
-def standard_deviation(array):
-    meanArray = mean(array)
-    for i in range(len(array)):
-        array[i] = (array[i] - meanArray) * (array[i] - meanArray)
-    variance = mean(array)
-    return(math.sqrt(variance))
 
 def calculate_confidence_intervalls(results):
 
     accuracies = []
     for i in range(len(results)):
         accuracies.append(results[i][0])
-    meanAccuracy = mean(accuracies)
-    standardDeviation = standard_deviation(accuracies)
+    meanAccuracy = statistics.mean(accuracies)
+    standardDeviation = statistics.stdev(accuracies)
     
     print("Mean accuracy: " + str(meanAccuracy))
     print("Confidence: [" + str(meanAccuracy - 1.96 * standardDeviation) + ", " + str(meanAccuracy + 1.96 * standardDeviation) +"]")
+    
+    
+    
+def probability_distribution_of_points(array):
+    return(list(zip(*array)))
+
+def calculate_variance_of_svm(results):
+    "results is an 2-dimensional array. It has N elements of array (of size m) which has a propability value of each point of the predciton dataset"
+    distribution_of_points = probability_distribution_of_points(results)
+    
+    standard_deviations = [None] * len(distribution_of_points)
+    
+    for index in range(len(distribution_of_points)):
+        std = statistics.stdev(distribution_of_points[index])
+        standard_deviations[index] = std
+        
+    return(statistics.mean(standard_deviations))
